0ba09e6c452ae71d205d8469438a3d8477018893
iskandar777-dar <54578330+iskandar777-dar@users.noreply.github.com>
2022-10-11 18:45:49 +0700

    Update __init__.py

--- EmikoRobot/db/__init__.py
+++ EmikoRobot/db/__init__.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
  
 def start() -> scoped_session:
-    engine = create_engine(config.DB_URL)
+    engine = create_engine(DB_URL)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
