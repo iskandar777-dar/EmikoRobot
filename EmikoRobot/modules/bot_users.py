ec0742a505a458ab3c795146eb05c679b599fe86
iskandar777-dar <54578330+iskandar777-dar@users.noreply.github.com>
2022-10-12 01:54:30 +0700

    Update bot_users.py

--- EmikoRobot/modules/bot_users.py
+++ EmikoRobot/modules/bot_users.py
@@ -1,13 +1,11 @@
 from pyrogram.types import Message
 from pyrogram import Client, filters
 
-from env import OWNER_ID
-from StringGenBot.db import SESSION
-from StringGenBot.db.users_sql import Users, num_users
+from EmikoRobot import OWNER_ID
+from EmikoRobot.db import SESSION
+from EmikoRobot.db.users_sql import Users, num_users
 
-
-@Client.on_message(~filters.service, group=1)
-async def users_sql(_, msg: Message):
+def users_sql(_, msg: Message):
     if msg.from_user:
         q = SESSION.query(Users).get(int(msg.from_user.id))
         if not q:
@@ -16,8 +14,10 @@ async def users_sql(_, msg: Message):
         else:
             SESSION.close()
 
+def _stats(_, msg: Message):
+    users = num_users()
+    msg.reply(f"» ᴄᴜʀʀᴇɴᴛ sᴛᴀᴛs ᴏғ sᴛʀɪɴɢ ɢᴇɴ ʙᴏᴛ :\n\n {users} ᴜsᴇʀs", quote=True)
 
-@Client.on_message(filters.user(OWNER_ID) & filters.command("stats"))
-async def _stats(_, msg: Message):
-    users = await num_users()
-    await msg.reply(f"» ᴄᴜʀʀᴇɴᴛ sᴛᴀᴛs ᴏғ sᴛʀɪɴɢ ɢᴇɴ ʙᴏᴛ :\n\n {users} ᴜsᴇʀs", quote=True)
+STATS_HANDLER = DisableAbleCommandHandler("stats", stats, run_async=True)
+
+dispatcher.add_handler(STATS_HANDLER)
