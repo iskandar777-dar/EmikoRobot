314136466fc19c817107c834cdc0408acf4e2cbb
iskandar777-dar <54578330+iskandar777-dar@users.noreply.github.com>
2022-10-12 02:05:52 +0700

    Update generate.py

--- EmikoRobot/modules/generate.py
+++ EmikoRobot/modules/generate.py
@@ -184,3 +184,10 @@ def cancelled(msg):
         return True
     else:
         return False
+        
+        
+GENERATE_SESSION_HANDLER = DisableAbleCommandHandler("generate", generate, run_async=True)
+CANCELLED_HANDLER = DisableAbleCommandHandler("cancelled", cancelled, run_async=True)
+
+dispatcher.add_handler(GENERATE_HANDLER)
+dispatcher.add_handler(CANCELLED_HANDLER)
