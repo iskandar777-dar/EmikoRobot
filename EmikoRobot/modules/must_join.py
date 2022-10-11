ff33eb0d57093659679a15dcf968112269fa0f29
iskandar777-dar <54578330+iskandar777-dar@users.noreply.github.com>
2022-10-12 02:10:41 +0700

    Update must_join.py

--- EmikoRobot/modules/must_join.py
+++ EmikoRobot/modules/must_join.py
@@ -1,24 +1,24 @@
-from env import MUST_JOIN
+from EmikoRobot import SUPPORT_CHAT
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 
 
 @Client.on_message(filters.incoming & filters.private, group=-1)
-async def must_join_channel(bot: Client, msg: Message):
-    if not MUST_JOIN:  # Not compulsory
+def must_join_channel(bot: Client, msg: Message):
+    if not SUPPORT_CHAT:  # Not compulsory
         return
     try:
         try:
-            await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
+            await bot.get_chat_member(SUPPORT_CHAT, msg.from_user.id)
         except UserNotParticipant:
-            if MUST_JOIN.isalpha():
-                link = "https://t.me/" + MUST_JOIN
+            if SUPPORT_CHAT.isalpha():
+                link = "https://t.me/" + SUPPORT_CHAT
             else:
-                chat_info = await bot.get_chat(MUST_JOIN)
+                chat_info = await bot.get_chat(SUPPORT_CHAT)
                 link = chat_info.invite_link
             try:
-                await msg.reply_photo(photo="https://telegra.ph/file/ba582d379f2586f227d66.png", caption=f"Gabung Group dibawah dulu lalu coba /start lagi!",
+                await msg.reply_photo(photo="https://telegra.ph/file/ba582d379f2586f227d66.png", caption=f"Gabung Group dibawah dulu lalu coba /string lagi!",
                     reply_markup=InlineKeyboardMarkup([
                         [InlineKeyboardButton("🥺 Gabung Group Sini 🥺", url=f"{link}")]
                     ])
@@ -27,4 +27,4 @@ async def must_join_channel(bot: Client, msg: Message):
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"Promote me as an admin in the MUST_JOIN chat : {MUST_JOIN} !")
+        print(f"Promote me as an admin in the SUPPORT_CHAT chat : {SUPPORT_CHAT} !")
